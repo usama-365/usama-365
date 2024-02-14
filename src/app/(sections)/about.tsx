a8138f3294e0d042252bf3b9bc257e0492ce9c66
usama-365 <usama-365@protonmail.com>
2024-02-15 00:34:47 +0500

    add proper about content

--- src/app/(sections)/about.tsx
+++ src/app/(sections)/about.tsx
@@ -3,27 +3,38 @@ import P from "@/components/typography/p";
 
 export default function About() {
   return (
-    <section className="px-4">
-      <H2 className="mx-auto w-max text-center">About Me</H2>
-      <P className="mx-auto max-w-2xl text-justify">
-        After graduating with a degree in Accounting, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned
-        full-stack web development. My favorite part of programming is the
-        problem-solving aspect. I love the feeling of finally figuring out a
-        solution to a problem. My core stack is React, Next.js, Node.js, and
-        MongoDB. I am also familiar with TypeScript and Prisma. I am always
-        looking to learn new technologies. I am currently looking for a
-        full-time position as a software developer.
+    <section className="mx-auto mb-12 max-w-2xl px-4 text-justify text-muted-foreground">
+      <H2 className="mx-auto w-max text-foreground">About Me</H2>
+      <P>
+        I leverage <strong>cutting-edge technology</strong> and a
+        <em> design-focused approach</em> to create
+        <em> user friendly, high performing</em> web, and mobile apps. Whether
+        you&apos;re seeking an <em>engaging user interface</em> or a
+        <em> robust backend solution</em>, I have the expertise to bring your
+        concepts to life.
       </P>
-      <P className="mx-auto max-w-2xl text-justify">
-        After graduating with a degree in Accounting, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned
-        full-stack web development. My favorite part of programming is the
-        problem-solving aspect. I love the feeling of finally figuring out a
-        solution to a problem. My core stack is React, Next.js, Node.js, and
-        MongoDB. I am also familiar with TypeScript and Prisma. I am always
-        looking to learn new technologies. I am currently looking for a
-        full-time position as a software developer.
+      <P>
+        My primary tools include <strong>JavaScript</strong>, particularly
+        <strong> React</strong> and <strong>NextJS</strong>. I specialize in
+        constructing <em>Single Page Applications</em> using <strong></strong>
+        React, utilizing managed backends like <strong>Firebase</strong>,
+        <strong> Supabase</strong>, or <strong>Appwrite</strong>. Additionally,
+        I develop custom backends in <strong>Javascript</strong>,
+        <strong> Python</strong>, or <strong>Go</strong>, personally ensuring
+        they align with your specific requirements.
+      </P>
+      <P>
+        Furthermore, I excel in developing <em>full-stack applications </em>
+        using <strong>React </strong>
+        with the <strong>MERN stack</strong> or <strong>NextJS</strong> with the
+        <strong> T3 stack</strong>, incorporating technologies such as
+        <strong> Tailwind</strong>, <strong>TypeScript</strong>, and
+        <strong> tRPC</strong>. Armed with a diverse skill set,<span> a </span>
+        <em className="underline">Computer Science degree</em>, and experience
+        collaborating with various clients, I am well-equipped to tackle
+        challenges and deliver optimal solutions tailored to your needs. Eager
+        for new projects and collaborations, let&apos;s discuss yours and
+        explore how I can contribute!
       </P>
     </section>
   );
