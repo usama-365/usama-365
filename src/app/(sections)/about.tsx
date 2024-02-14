cc2139d2a8ef214b9bf0aedf3fc3eb337bf09359
Usama <usama-365@protonmail.com>
2024-02-14 22:13:38 +0500

    start creating the about section

---
+++ src/app/(sections)/about.tsx
@@ -0,0 +1,30 @@
+import H2 from "@/components/typography/h2";
+import P from "@/components/typography/p";
+
+export default function About() {
+  return (
+    <section className="px-4">
+      <H2 className="mx-auto w-max text-center">About Me</H2>
+      <P className="mx-auto max-w-2xl text-justify">
+        After graduating with a degree in Accounting, I decided to pursue my
+        passion for programming. I enrolled in a coding bootcamp and learned
+        full-stack web development. My favorite part of programming is the
+        problem-solving aspect. I love the feeling of finally figuring out a
+        solution to a problem. My core stack is React, Next.js, Node.js, and
+        MongoDB. I am also familiar with TypeScript and Prisma. I am always
+        looking to learn new technologies. I am currently looking for a
+        full-time position as a software developer.
+      </P>
+      <P className="mx-auto max-w-2xl text-justify">
+        After graduating with a degree in Accounting, I decided to pursue my
+        passion for programming. I enrolled in a coding bootcamp and learned
+        full-stack web development. My favorite part of programming is the
+        problem-solving aspect. I love the feeling of finally figuring out a
+        solution to a problem. My core stack is React, Next.js, Node.js, and
+        MongoDB. I am also familiar with TypeScript and Prisma. I am always
+        looking to learn new technologies. I am currently looking for a
+        full-time position as a software developer.
+      </P>
+    </section>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
 import Header from "@/app/(sections)/header";
+import About from "./(sections)/about";
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <About />
     </main>
   );
 }
